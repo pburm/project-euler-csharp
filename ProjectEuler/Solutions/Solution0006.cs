2a3090b3087c6c54f4ef7de1c6adebfce3b93ad8
Patrick Burm <pat.burm@gmail.com>
2018-11-06 18:24:21 -0700

    add 6,7,8

---
+++ ProjectEuler/Solutions/Solution0006.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Solutions
+{
+    public class Solution0006 : ISolution
+    {
+        public void Solve()
+        {
+            var sum = 0;
+            var sumOfSquares = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                sum += i;
+                sumOfSquares += (int)Math.Pow(i, 2);
+            }
+            var result = Math.Pow(sum, 2) - sumOfSquares;
+            Console.WriteLine($"The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is {result}");
+        }
+    }
+}
---
+++ ProjectEuler/Solutions/Solution0007.cs
@@ -0,0 +1,30 @@
+﻿using ProjectEuler.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Solutions
+{
+    public class Solution0007 : ISolution
+    {
+        public void Solve()
+        {
+            var result = 13;
+            var temp = result;
+            var nth = 6;
+            while (nth < 10001)
+            {
+                temp += 2;
+                if (temp.IsPrime())
+                {
+                    result = temp;
+                    nth++;
+                }
+                    
+            }
+            Console.WriteLine($"The 10001st prime number is {result}");
+        }
+    }
+}
